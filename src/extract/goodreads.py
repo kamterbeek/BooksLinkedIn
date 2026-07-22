ce3585121224afdb4864dbb1e77b6a76812b78f2
Kassandra ter Beek <kam.terbeek@gmail.com>
2026-07-21 20:35:06 -0600

    finish goodreads.py

--- src/extract/goodreads.py
+++ src/extract/goodreads.py
@@ -5,3 +5,43 @@ import pandas as pd
 from models import Book
 from validator import validate_book
 
+DATA_PATH = (
+    Path(__file__)
+    .resolve()
+    .parents[2]
+    / "data"
+    / "raw"
+    / "goodreads_library_export.csv"
+)
+
+def load_books():
+    df = pd.read_csv(DATA_PATH)
+
+    books = []
+
+    for _, row in df.iterrows():
+        book = Book(
+            book_id=row["Book Id"],
+            title=row["Title"],
+            author=row["Author"],
+            isbn=row.get("ISBN"),
+            isbn13=row.get("ISBN13"),
+            rating=row.get("My Rating"),
+            pages=row.get("Number of Pages"),
+            year_published=row.get("Year Published"),
+            date_added=row.get("Date Added"),
+            date_read=row.get("Date Read"),
+            shelf=row.get("Exclusive Shelf"),
+            review=row.get("Review"),
+        )
+        if validate_book(book):
+            books.append(book)
+    return books
+
+if __name__ == "__main__":
+
+    books = load_books()
+
+    print(f"Loaded {len(books)} books.")
+
+    print(books[0])
