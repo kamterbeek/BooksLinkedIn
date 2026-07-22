53ca61128e5da8c40f5e53b7614c6b54d3496f41
Kassandra ter Beek <kam.terbeek@gmail.com>
2026-07-21 20:05:02 -0600

    update validator

--- src/extract/validator.py
+++ src/extract/validator.py
@@ -2,6 +2,6 @@ from models import Book
 
 def validate_book(book: Book) -> bool:
     return (
-        len(book.title) > 0 
-        and len(book.author) > 0
+      len(book.title.strip()) > 0
+        and len(book.author.strip()) > 0
     )
