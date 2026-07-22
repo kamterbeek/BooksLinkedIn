a025dd80fce24ad7a7dea06daea944a5f7e63bfa
Kassandra ter Beek <kam.terbeek@gmail.com>
2026-07-21 19:27:16 -0600

    finish models.py

--- src/extract/models.py
+++ src/extract/models.py
@@ -5,3 +5,18 @@ class book(BaseModel):
     book_id: int
     title: str
     author: str
+
+    isbn: Optional[str] = None
+    isbn13: Optional[str] = None
+
+    rating: Optional[int] = None
+    pages: Optional[int] = None
+
+    year_published: Optional[int] = None
+
+    date_added: Optional[str] = None
+    date_read: Optional[str] = None
+
+    shelf: Optional[str] = None
+
+    review: Optional[str] = None
