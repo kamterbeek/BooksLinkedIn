4eac74859f7369f397742963c5da93bf1752cfd3
Kassandra ter Beek <kam.terbeek@gmail.com>
2026-07-22 21:43:13 -0600

    code

--- src/transform/cleaner.py
+++ src/transform/cleaner.py
@@ -1,1 +1,16 @@
 import pandas as pd
+
+def clean_value(value):
+    """
+    Convert Pandas NaN values into Python None.
+    """
+    if pd.isna(value):
+        return None
+    return value
+
+def clean_isbn(value):
+    """Remove Goodreads Excel Formatting from ISBN values."""
+    value = clean_value(value)
+
+    if value is None:
+        return None
